65b01a7a7841f30d2085f4b428f983b130e5ec52
SATATA <sadiqul842@gmail.com>
2020-10-08 22:11:07 +0600

    post auhtor page added

---
+++ my-app/src/features/features/posts/PostAuthor.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUserById } from '../users/usersSlice';
+
+
+export const PostAuhtor=({userId})=>{
+
+  const author = useSelector((state)=> selectUserById(state, userId));
+
+  return <span>
+
+      by {author ? author.name : 'Unknown Author' } 
+  </span>
+
+
+  
+
+};
+    
+    
+    
